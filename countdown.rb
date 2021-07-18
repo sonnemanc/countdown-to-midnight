f13f2776a434d65285f5f57fed6f1c174bad7368
Corban <sonnemanc@gmail.com>
2021-07-18 13:46:52 -0600

    Done with Assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(n)
+    n = 10
+    until n == 0
+    puts"#{n} SECOND(S)!"
+    n -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+    n = 10
+    until n == 0
+    puts"#{n} SECOND(S)!"
+    n -= 1
+    sleep 1
+    end
+    "HAPPY NEW YEAR!"
 end
